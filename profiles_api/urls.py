cafd3734c1f3b775c133a839a1fa369caf095be8
olaide <olaide.olanipekun@yahoo.com>
2022-03-20 12:22:39 +0000

    add comment

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
+"""API ViewSet"""
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
